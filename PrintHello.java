b8bba7ef1a98c584454455682297c9a3baba1d9a
Tamilgithub1 <31219547+Tamilgithub1@users.noreply.github.com>
2018-08-20 15:39:47 +0530

    Create PrintHello.java

---
+++ PrintHello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class test {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter a number :");
+	int n=in.nextInt();
+	for(int i=0;i<n;i++) {
+		System.out.println("Hello");
+	}
+}
+}
